17bac1bbe61f8f50fca438e767c0feddf1b905af
Bohdan Oleksiuk <bohdan.oleksiuk@ecdevstudio.com>
2024-05-08 15:20:37 +0300

    rm subscribes

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const { createServer } = require("http");
 const express = require("express");
-const { execute, subscribe } = require("graphql");
 const { ApolloServer } = require("apollo-server-express");
-const { SubscriptionServer } = require("subscriptions-transport-ws");
 const { makeExecutableSchema } = require("@graphql-tools/schema");
 const cors = require('cors');
 
@@ -28,7 +26,6 @@ const cors = require('cors');
   const server = new ApolloServer({
     schema,
     context: contextMiddleware,
-    // plugins: [ApolloServerPluginLandingPageDisabled()],
     playground: true,
     introspection: true,
   });
@@ -36,17 +33,9 @@ const cors = require('cors');
   await server.start();
   server.applyMiddleware({ app });
 
-  SubscriptionServer.create(
-    { schema, execute, subscribe },
-    { server: httpServer, path: server.graphqlPath }
-  );
-
   httpServer.listen(PORT, () => {
     console.log(
       `🚀 Query endpoint ready at http://localhost:${PORT}${server.graphqlPath}`
-    );
-    console.log(
-      `🚀 Subscription endpoint ready at ws://localhost:${PORT}${server.graphqlPath}`
     );
   });
 
