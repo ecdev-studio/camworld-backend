45de0ffc9789e31a823b17bb90325712e6d1ebab
Bohdan Oleksiuk <bohdan.oleksiuk@ecdevstudio.com>
2024-05-08 16:02:24 +0300

    fix port

--- utils/database.js
+++ utils/database.js
@@ -5,7 +5,7 @@ const DB_NAME = process.env.DB_NAME;
 const USER_NAME = process.env.DB_USER;
 const PASSWORD = process.env.DB_PASS;
 const HOST = process.env.DB_HOST;
-const PORT = process.env.DB_PORT;
+const PORT = 5432;
 
 const sequelizeDB = new Sequelize(DB_NAME, USER_NAME, PASSWORD, {
   host: HOST,
